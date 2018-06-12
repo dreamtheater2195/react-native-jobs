ab2c9e28d00f5111df174ba6f3e2a83086253748
Quan Minh Pham <dreamtheater2195@gmail.com>
2018-06-12 10:29:54 +0700

    Finish welcome screen

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingScreen from './screens/SettingScreen';
+import ReviewScreen from './screens/ReviewScreen';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = createBottomTabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: createBottomTabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: createStackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingScreen }
+            })
+          }
+        })
+      }
+    });
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <MainNavigator />
     );
   }
 }
